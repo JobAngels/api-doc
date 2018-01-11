6dc013267929874808d8840f6e320db2d2c03ec4
Peter Kovalčík <peter@jobangels.co>
2018-01-11 11:08:57 +0100

    php -f combinate.php => kompinute vsetky .md subory z markdown do jedneho apiary.apib a tiez aj do README.md kvoli dokumentacii a mokup serveru

---
+++ combinate.php
@@ -0,0 +1,17 @@
+<?php
+require_once(__DIR__ . '/vendor/autoload.php');
+
+
+$apiary = __DIR__ . '/apiary.apib';
+$readme = __DIR__ . '/apiary.apib';
+file_put_contents($apiary, '');
+
+foreach (\Nette\Utils\Finder::findFiles('*.md')->in(__DIR__ . '/markdown/') as $key=>$file) {
+  $content = file_get_contents($key);
+  file_put_contents($apiary, $content ."\n\n", FILE_APPEND);
+}
+
+file_put_contents($readme, file_get_contents($apiary));
+
+
+
